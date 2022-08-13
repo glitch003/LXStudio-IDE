fb9fa33b829e37a9d4ebaea6f29aea3eecf87674
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-08-12 22:47:23 -0600

    added outputs

--- generatePattern.py
+++ generatePattern.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 
 # provide total number of LEDs and path to LXStudio-IDE project directory
 total_leds = 900
-lxstudio_path = 'LXStudio-IDE-master'
+lxstudio_path = './'
 
 # generate coordinates
 # z = 2 * PI * current_led_num / total_leds
@@ -29,6 +29,7 @@ lxf_dict = OrderedDict()
 lxf_dict['label'] = 'Spiral-%d' % total_leds
 lxf_dict['tags'] = ['custom', 'glorb', 'spiral']
 lxf_dict['components'] = components_data
+lxf_dict["outputs"] = [{ "protocol": "artnet", "universe": 10, "host": "192.168.1.3", "start": 0, "num": 900 }]
 
 with open('{0}/Fixtures/spiral-{1}.lxf'.format(lxstudio_path, total_leds), 'w') as f:
 	json.dump(lxf_dict, f, indent=2)
