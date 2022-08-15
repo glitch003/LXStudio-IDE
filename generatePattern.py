98fe4dc57c8db34461954bf457f280ef3f79a266
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-08-15 01:09:15 -0600

    it works and kinda looks likg glorb

--- generatePattern.py
+++ generatePattern.py
@@ -6,7 +6,6 @@ from collections import OrderedDict
 
 # provide total number of LEDs and path to LXStudio-IDE project directory
 total_leds = 900
-lxstudio_path = 'Documents/LXStudio-IDE-master'
 
 # generate coordinates
 # z = 2 * PI * current_led_num / total_leds
@@ -15,9 +14,10 @@ lxstudio_path = 'Documents/LXStudio-IDE-master'
 components_data = []
 point_data = OrderedDict()
 point_data['type'] = 'points'
-point_data['coords'] = [OrderedDict()]
+point_data['coords'] = []
+scalar = 270
 for n in range(total_leds):
-	z = 2 * math.pi * n / total_leds
+	z = (2 * math.pi * n / total_leds) * scalar
 	x = math.cos(6 * z)
 	y = math.sin(6 * z)
 	coords = {'x': x, 'y': y, 'z': z}
@@ -30,5 +30,5 @@ lxf_dict['label'] = 'Spiral-%d' % total_leds
 lxf_dict['tags'] = ['custom', 'glorb', 'spiral']
 lxf_dict['components'] = components_data
 
-with open('spiral-{0}.lxf'.format(total_leds), 'w+') as f:
+with open('Fixtures/spiral-{0}.lxf'.format(total_leds), 'w+') as f:
 	json.dump(lxf_dict, f, indent=2)
